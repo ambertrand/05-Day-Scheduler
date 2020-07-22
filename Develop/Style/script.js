a5bfd2b81ff03137415119c46ce9fb83633a927b
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-07-21 20:23:36 -0400

    adding psuedo code to js

--- Develop/Style/script.js
+++ Develop/Style/script.js
@@ -1,7 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-const dateDisplay = $("#currentDay");
-const currentDate = moment().format("MMMM DD, YYYY");
+    // Displays current date in    
+    const dateDisplay = $("#currentDay");
+    const currentDate = moment().format("MMMM DD, YYYY");
 
-dateDisplay.text(currentDate);
+    dateDisplay.text(currentDate);
+
+    let workSchedule = [{
+        
+    }]
+
+
+
 });
+
+
+
+
+// Psuedo code
+
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
