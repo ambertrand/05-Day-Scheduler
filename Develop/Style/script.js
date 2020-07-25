efdab031108cb3e2e9788e077993c72c845338b5
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-07-24 23:06:38 -0400

    problem solving js

--- Develop/Style/script.js
+++ Develop/Style/script.js
@@ -3,26 +3,59 @@ $(document).ready(function () {
     // Displays current date in    
     const dateDisplay = $("#currentDay");
     const currentDate = moment().format("LLLL");
-    
+
     dateDisplay.text(currentDate);
 
-    const textInput = document.querySelector(".textInput")
-    let input = [];
+    let daySchedule = [{
+        time: 9,
+        input: ""
+    },
+    {
+        time: 10,
+        input: ""
+    },
+    {
+        time: 11,
+        input: ""
+    },
+    {
+        time: 12,
+        input: ""
+    },
+    {
+        time: 1,
+        input: ""
+    },
+    {
+        time: 2,
+        input: ""
+    },
+    {
+        time: 3,
+        input: ""
+    },
+    {
+        time: 4,
+        input: ""
+    },
+    {
+        time: 5,
+        input: ""
+    },
+    ];
 
-    // When text is inputed this function sends to local storage when save button clicked
-    function localStorageInput() {
-        input = JSON.parse(localStorage.getItem("textInput"));
-        const text = $(".textInput").val();
-        let scheduleInput = { text };
-        if (!input) {
-            input = [];
-        }
-        input.push(scheduleInput);
-        localStorage.setItem("textInput", JSON.stringify(input));
-    }
+    // function localStorageInput() {
+    //     if (!localStorage.getItem('daySchedule')) {
+    //         localStorage.setItem('daySchedule', JSON.stringify(daySchedule));  
+    //         console.log('daySchedule')  
+    //     }
+    // }
+
+
+
 
         // Color coded text area
-        $("textarea").each(function() {
+        $("textarea").each(function () {
             const textAreaHour = parseInt($(this).attr("value"));
             const timeBlock = parseInt(moment().format("H"));
             if (timeBlock > textAreaHour) {
@@ -35,14 +68,15 @@ $(document).ready(function () {
         });
 
 
-    $(".saveBtn").on("click", function () {
-        // console.log("clicked");
-        localStorageInput();
-    })
+        $(".saveBtn").on("click", function () {
+            // console.log("clicked");
+            localStorageInput();
+        })
 
-    
+
 
 });
+
 
 
 
@@ -51,17 +85,24 @@ $(document).ready(function () {
 // When button is clicked input sent to local storage
 // When input added to Pull from LS to append to text area
 // Delete input removes from LS
-// Add time from moment so color code blocks work based on time
 
 
 // Requirements
 
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
+
+
+// function localStorageInput() {
+//     input = JSON.parse(localStorage.getItem("textInput"));
+//     const text = $(".textInput").val();
+//     let scheduleInput = { text };
+//     if (!input) {
+//         input = [];
+//     }
+// }
+
+//     input.push(scheduleInput);
+//     localStorage.setItem("textInput", JSON.stringify(input));
